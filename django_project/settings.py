7508613fbf40ebf2716ad4148b327d4be4f1444a
KayleeB <k.bennett0000@gmail.com>
2022-04-23 20:44:07 -0700

    Testing Photo Gallery for PythonAnywhere.

--- django_project/settings.py
+++ django_project/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
